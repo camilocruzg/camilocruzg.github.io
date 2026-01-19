d9033eef69eac5f7aad1e1b82544a0ded27dc4ee
Camilo Cruz <camilocruzg@gmail.com>
2026-01-19 06:46:32 +0000

    Updates

--- _next/static/sqcqVIaFa19mGM-_HGWVm/_buildManifest.js
+++ _next/static/sqcqVIaFa19mGM-_HGWVm/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/personal_website//_next/:path+",
+        "destination": "/personal_website/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
